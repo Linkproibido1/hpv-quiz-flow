fc44ab48d301e37441cbe34d160f7ff60268125e
Linkproibido1 <usobio@proton.me>
2025-04-18 17:38:04 -0300

    Update WelcomeScreen.tsx

--- src/components/WelcomeScreen.tsx
+++ src/components/WelcomeScreen.tsx
@@ -30,7 +30,6 @@ export const WelcomeScreen = ({ onStart }: WelcomeScreenProps) => {
 
       <p className="text-gray-600">
 🔥 Selecionamos pessoas para testar nosso tratamento exclusivo com Barbatimão, que já eliminou as verrugas do HPV em mais de 23.000 pessoas. Quer se livrar disso de vez?
-      
       <div className="bg-yellow-50 border border-yellow-200 p-3 rounded-lg flex items-center gap-3">
         <AlertTriangle className="text-yellow-600 flex-shrink-0" />
         <p className="text-yellow-800 text-sm">
